120e5d6fc0722a58d216765a10033d1943e1bb94
Grady Robbins <gradyrobbins13@gmail.com>
2020-07-21 16:28:39 -0500

    creating a component that shows the details of the questions available in your backend   &&   Routing Users with React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
+import Question from './Question/Question';
 import Questions from './Questions/Questions';
+
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
-        <Questions/>
-        <p>Work in progress.</p>
+        <Route exact path='/' component={Questions}/>
+        <Route exact path='/question/:questionId' component={Question}/>
       </div>
     );
   }
